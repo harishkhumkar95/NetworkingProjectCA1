a61d7c98d04cf50f1b2b411a570b8bba72a52cbe
harishkhumkar95@gmail.com <harishkhumkar95@gmail.com>
2024-11-26 19:00:55 +0000

    testchange

--- codes/sample_app/app.py
+++ codes/sample_app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "welcome to my new application"
+    return "welcome to my new"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
